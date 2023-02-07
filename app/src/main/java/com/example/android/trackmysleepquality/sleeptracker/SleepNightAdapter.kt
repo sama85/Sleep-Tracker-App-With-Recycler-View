3e7fa092634f3f49c23192d973de4acf6648f4c1
sama85 <samayasser8520@gmail.com>
2023-02-07 15:05:31 +0200

    used data binding for view holder with binding adapters

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -79,28 +79,9 @@ class SleepNightAdapter : androidx.recyclerview.widget.ListAdapter<SleepNight, R
         }
 
         fun bind(item: SleepNight) {
-            val res = itemView.context.resources
-            if (item.sleepQuality <= 1)
-                binding.sleepQualityText.setTextColor(Color.RED)
-            //must reset color to black for high qualities bec this holder is reused and could be
-            //modified by previous data
-            else
-                binding.sleepQualityText.setTextColor(Color.BLACK)
-
-            binding.sleepQualityText.text = convertNumericQualityToString(item.sleepQuality, res)
-            binding.sleepLength.text =
-                convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, res)
-            binding.sleepQualityImage.setImageResource(
-                when (item.sleepQuality) {
-                    0 -> R.drawable.ic_sleep_0
-                    1 -> R.drawable.ic_sleep_1
-                    2 -> R.drawable.ic_sleep_2
-                    3 -> R.drawable.ic_sleep_3
-                    4 -> R.drawable.ic_sleep_4
-                    5 -> R.drawable.ic_sleep_5
-                    else -> R.drawable.ic_sleep_active
-                }
-            )
+           binding.sleepNight = item
+           //WHAT DOES IT DO?
+           binding.executePendingBindings()
         }
     }
 
